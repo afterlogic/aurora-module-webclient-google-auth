cd5765288c4dab81982051cf03d7a02152824119
nadine-shi <nadine@afterlogic.com>
2016-07-29 16:42:51 +0300

    AdminPanel - Settings for GoogleAuth and DropBoxAuth Modules

--- module.php
+++ module.php
@@ -5,6 +5,7 @@ class GoogleAuthModule extends AApiModule
 	protected $sService = 'google';
 	
 	protected $aSettingsMap = array(
+		'EnableModule' => array(false, 'bool'),
 		'Id' => array('', 'string'),
 		'Secret' => array('', 'string')
 	);
@@ -18,6 +19,8 @@ class GoogleAuthModule extends AApiModule
 		$this->incClass('connector');
 		$this->subscribeEvent('ExternalServicesAction', array($this, 'onExternalServicesAction'));
 		$this->subscribeEvent('GetServices', array($this, 'onGetServices'));
+		$this->subscribeEvent('GetServicesSettings', array($this, 'onGetServicesSettings'));
+		$this->subscribeEvent('UpdateServicesSettings', array($this, 'onUpdateServicesSettings'));
 	}
 	
 	/**
@@ -27,7 +30,49 @@ class GoogleAuthModule extends AApiModule
 	 */
 	public function onGetServices(&$aServices)
 	{
-		$aServices[] = $this->sService;
+		if ($this->getConfig('EnableModule', false))
+		{
+			$aServices[] = $this->sService;
+		}
+	}
+	
+	/**
+	 * Adds service settings to array passed by reference.
+	 * 
+	 * @param array $aServices Array with services settings passed by reference.
+	 */
+	public function onGetServicesSettings(&$aServices)
+	{
+		$aServices[] = array(
+			'Name' => $this->sService,
+			'DisplayName' => $this->GetName(),
+			'EnableModule' => $this->getConfig('EnableModule', false),
+			'Id' => $this->getConfig('Id', ''),
+			'Secret' => $this->getConfig('Secret', '')
+		);
+	}
+	
+	/**
+	 * Updates service settings.
+	 * 
+	 * @param array $aServices Array with new values for service settings.
+	 * 
+	 * @throws \System\Exceptions\ClientException
+	 */
+	public function onUpdateServicesSettings($aServices)
+	{
+		try
+		{
+			$aSettings = $aServices[$this->sService];
+			$this->setConfig('EnableModule', $aSettings['EnableModule']);
+			$this->setConfig('Id', $aSettings['Id']);
+			$this->setConfig('Secret', $aSettings['Secret']);
+			$this->saveModuleConfig();
+		}
+		catch (Exception $ex)
+		{
+			throw new \System\Exceptions\ClientException(\System\Notifications::CanNotSaveSettings);
+		}
 	}
 	
 	public function onExternalServicesAction($sService, &$mResult)
