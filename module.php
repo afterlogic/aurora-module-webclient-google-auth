96beb35dc2ab03da59cd133e018f717ff3a8ef95
sash04ek <a.ovcharov@gmail.com>
2016-10-14 16:13:39 +0300

    bug fixes

--- module.php
+++ module.php
@@ -52,12 +52,12 @@ class GoogleAuthWebclientModule extends AApiModule
 	 * Passes data to connect to service.
 	 * 
 	 * @ignore
-	 * @param string $sService Service type to verify if data should be passed.
+	 * @param string $aArgs Service type to verify if data should be passed.
 	 * @param boolean|array $mResult variable passed by reference to take the result.
 	 */
-	public function onOAuthIntegratorAction($sService, &$mResult)
+	public function onOAuthIntegratorAction($aArgs, &$mResult)
 	{
-		if ($sService === $this->sService)
+		if ($aArgs['Service'] === $this->sService)
 		{
 			$mResult = false;
 			$oConnector = new COAuthIntegratorConnectorGoogle($this);
