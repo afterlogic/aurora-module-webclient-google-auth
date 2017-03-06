eb4d710961ab5a6353bb4c577bb42705119f7bdd
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:48:18 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\GoogleAuthWebclient;
 
-class GoogleAuthWebclientModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	protected $sService = 'google';
 	
